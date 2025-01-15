344aed55940907a98ac502824e7a6a40d65de005
Artyom <art002272@gmail.com>
2025-01-15 14:58:41 +0500

    develop | add migrations

---
+++ database/migrations/2025_01_15_083247_create_politicians_orders_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('politicians_orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('politician_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('politicians_orders');
+    }
+};
---
+++ database/migrations/2025_01_15_084047_create_interviewees_politicians_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('interviewees_politicians', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('interviewee_id')->constrained()->onDelete('cascade');
+            $table->foreignId('politician_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('interviewees_politicians');
+    }
+};
