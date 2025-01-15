e495b103fcd00b6a69acc1aadd21f2bb06e79db9
Artyom <art002272@gmail.com>
2025-01-15 15:16:22 +0500

    develop | add models

---
+++ app/Models/Politician.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Politician extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'second_name',
+        'first_name',
+        'sex',
+        'age',
+        'territory_id',
+    ];
+}
